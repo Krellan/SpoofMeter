fe44ded3c9dcf8aa9c800a6cef6abbe85831e001
Josh Lehan <krellan@krellan.net>
2025-08-12 16:23:43 -0700

    Cleaning up C++ headers
    Thanks, CLion, for better warnings!
    
    Changed headers to use C++ names
    Added forgotten cstdint header
    
    Signed-off-by: Josh Lehan <krellan@krellan.net>

--- spoofmeter_common.h
+++ spoofmeter_common.h
@@ -13,9 +13,10 @@
 // The following are general utilities,
 // not necessarily specific to SpoofMeter.
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cstdint>
 
 #include <string>
 
